18ccd4cb5b791af9d165c38264587835db1eb921
Santiago <scoulchinsky@gmail.com>
2023-12-14 17:02:37 -0300

    e

--- src/routes/autos.js
+++ src/routes/autos.js
@@ -24,8 +24,8 @@ router.post('/', (req, res) => {
         autos.push(newAuto);
         
         const nuevoContenido = JSON.stringify(autos);
-        fs.writeFileSync('src/sample.json', autos, 'utf-8');
-        fs.writeFileSync(url, autos, 'utf-8');
+        fs.writeFileSync('src/sample.json', nuevoContenido, 'utf-8');
+        /* fs.writeFileSync(url, nuevoContenido, 'utf-8'); */
         res.json(nuevoContenido);
         
         
